06be2c934d3d59c4cb70f1133cd3e944e766efe4
Elvin Shrestha <37188510+elwyncrestha@users.noreply.github.com>
2018-08-11 16:57:26 +0545

    SQL file and ER-diagram added

---
+++ requisites/database_MySQL/Queries.sql
@@ -0,0 +1,55 @@
+use bookstoredb;
+
+create table BookCategory
+(
+	categoryId int not null auto_increment primary key,
+    categoryName varchar(255) not null
+);
+
+create table Book
+(
+	bookId int not null auto_increment primary key,
+    bookName varchar(255) not null,
+    bookPrice float not null,
+    bookAuthor varchar(255) not null,
+    bookPublisher varchar(255),
+    bookPublishedDate date,
+    bookImageURL varchar(500)
+);
+
+create table User
+(
+	userId int not null auto_increment primary key,
+    userFName varchar(255),
+    userLName varchar(255),
+    userAddress varchar(255),
+    userDOB date,
+    userPhoneNo varchar(20),
+    userGender char(1)
+);
+
+create table Book_BookCategory
+(
+	bookId int not null,
+    categoryId int not null,
+    constraint fk_Book_BookCategory_bookId foreign key (bookId) references Book(bookId),
+    constraint fk_Book_BookCategory_categoryId foreign key (categoryId) references BookCategory(categoryId)
+);
+
+create table Book_User
+(
+	bookId int not null,
+	userId int not null,
+    constraint fk_Book_User_bookId foreign key (bookId) references Book(bookId),
+    constraint fk_Book_user_userId foreign key (userId) references User(userId)
+);
+
+create table UserCategoryInterest
+(
+	categoryId int not null,
+	userId int not null,
+    constraint fk_UserCategoryInterest_categoryId foreign key (categoryId) references BookCategory(categoryId),
+    constraint fk_UserCategoryInterest_userId foreign key (userId) references User(userId)
+);
+
+show tables;
