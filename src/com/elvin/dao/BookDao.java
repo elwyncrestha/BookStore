a14c27b44cb96bc2f02612069ac59a48badce315
Elvin Shrestha <37188510+elwyncrestha@users.noreply.github.com>
2018-08-18 21:03:04 +0545

    Book Update and Delete
    -book edit and delete
    -book category delete modal added

--- src/com/elvin/dao/BookDao.java
+++ src/com/elvin/dao/BookDao.java
@@ -28,14 +28,171 @@ public class BookDao {
 				preparedStatement.setBinaryStream(6, book.getBookImageURL());
 				preparedStatement.setInt(7, book.getBookQuantity());
 				preparedStatement.setString(8, book.getBookImageName());
-				
+
 				int i = preparedStatement.executeUpdate();
-				if(i != 0)
-				{
+				if (i != 0) {
 					status = true;
 					System.out.println("Book added !!!");
 				}
 			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					preparedStatement.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return status;
+	}
+
+	public static ArrayList<Book> displayAll() {
+		ArrayList<Book> arrayList = new ArrayList<>();
+		Connection connection = MyConnection.connect();
+		PreparedStatement preparedStatement = null;
+
+		if (connection != null) {
+			String sql = "select bookId,bookName,bookPrice,bookAuthor,bookPublisher,bookPublishedDate,bookImageURL,bookQuantity,bookImageName from book";
+			try {
+				preparedStatement = connection.prepareStatement(sql);
+
+				ResultSet resultSet = preparedStatement.executeQuery();
+				while (resultSet.next()) {
+					int bookId = resultSet.getInt(1);
+					String bookName = resultSet.getString(2);
+					float bookPrice = Float.parseFloat(resultSet.getString(3));
+					String bookAuthor = resultSet.getString(4);
+					String bookPublisher = resultSet.getString(5);
+					String bookPublishedDate = resultSet.getString(6);
+					InputStream bookImage = resultSet.getBinaryStream(7);
+					int bookQuantity = resultSet.getInt(8);
+					String bookImageName = resultSet.getString(9);
+
+					arrayList.add(new Book(bookId, bookName, bookPrice, bookAuthor, bookPublisher, bookPublishedDate,
+							bookImage, bookQuantity, bookImageName));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					preparedStatement.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return arrayList;
+	}
+
+	public static Book selectById(int id) {
+		Connection connection = MyConnection.connect();
+		PreparedStatement preparedStatement = null;
+		Book book = null;
+
+		if (connection != null) {
+			String sql = "select bookId,bookName,bookPrice,bookAuthor,bookPublisher,bookPublishedDate,bookImageURL,bookQuantity,bookImageName from book where bookId=?";
+			try {
+				preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setInt(1, id);
+				ResultSet resultSet = preparedStatement.executeQuery();
+
+				while (resultSet.next()) {
+					int bookId = resultSet.getInt(1);
+					String bookName = resultSet.getString(2);
+					float bookPrice = Float.parseFloat(resultSet.getString(3));
+					String bookAuthor = resultSet.getString(4);
+					String bookPublisher = resultSet.getString(5);
+					String bookPublishedDate = resultSet.getString(6);
+					InputStream bookImage = resultSet.getBinaryStream(7);
+					int bookQuantity = resultSet.getInt(8);
+					String bookImageName = resultSet.getString(9);
+
+					book = new Book(bookId, bookName, bookPrice, bookAuthor, bookPublisher, bookPublishedDate,
+							bookImage, bookQuantity, bookImageName);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					preparedStatement.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return book;
+	}
+
+	public static boolean update(Book book) {
+		Connection connection = MyConnection.connect();
+		PreparedStatement preparedStatement = null;
+		boolean status = false;
+
+		if (connection != null) {
+			String sql = null;
+			if (book.getBookImageName().length() == 0) {
+				sql = "update book set bookName=?,bookPrice=?,bookAuthor=?,bookPublisher=?,bookPublishedDate=?,bookQuantity=bookQuantity+? where bookId=?";
+			} else {
+				sql = "update book set bookName=?,bookPrice=?,bookAuthor=?,bookPublisher=?,bookPublishedDate=?,bookQuantity=bookQuantity+?,bookImageURL=?,bookImageName=? where bookId=?";
+			}
+
+			try {
+				preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setString(1, book.getBookName());
+				preparedStatement.setFloat(2, book.getBookPrice());
+				preparedStatement.setString(3, book.getBookAuthor());
+				preparedStatement.setString(4, book.getBookPublisher());
+				preparedStatement.setString(5, book.getBookPublishedDate());
+				preparedStatement.setInt(6, book.getBookQuantity());
+				if (book.getBookImageName().length() > 0) {
+					preparedStatement.setBinaryStream(7, book.getBookImageURL());
+					preparedStatement.setString(8, book.getBookImageName());
+					preparedStatement.setInt(9, book.getBookId());
+				} else {
+					preparedStatement.setInt(7, book.getBookId());
+				}
+
+				int i = preparedStatement.executeUpdate();
+				if (i != 0) {
+					status = true;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					preparedStatement.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return status;
+	}
+
+	public static boolean delete(int id) {
+		boolean status = false;
+		Connection connection = MyConnection.connect();
+		PreparedStatement preparedStatement = null;
+		
+		if(connection != null)
+		{
+			String sql = "delete from book where bookId=?";
+			try
+			{
+				preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setInt(1, id);
+				int i = preparedStatement.executeUpdate();
+				if(i != 0)
+				{
+					status = true;
+				}
+			}catch(SQLException e)
+			{
 				e.printStackTrace();
 			}
 			finally
@@ -52,100 +209,5 @@ public class BookDao {
 			}
 		}
 		return status;
-	}
-	
-	public static ArrayList<Book> displayAll()
-	{
-		ArrayList<Book> arrayList = new ArrayList<>();
-		Connection connection = MyConnection.connect();
-		PreparedStatement preparedStatement = null;
-
-		if (connection != null) {
-			String sql = "select bookId,bookName,bookPrice,bookAuthor,bookPublisher,bookPublishedDate,bookImageURL,bookQuantity,bookImageName from book";
-			try {
-				preparedStatement = connection.prepareStatement(sql);
-				
-				ResultSet resultSet = preparedStatement.executeQuery();
-				while(resultSet.next())
-				{
-					int bookId = resultSet.getInt(1);
-					String bookName = resultSet.getString(2);
-					float bookPrice = Float.parseFloat(resultSet.getString(3));
-					String bookAuthor = resultSet.getString(4);
-					String bookPublisher = resultSet.getString(5);
-					String bookPublishedDate = resultSet.getString(6);
-					InputStream bookImage = resultSet.getBinaryStream(7);
-					int bookQuantity = resultSet.getInt(8);
-					String bookImageName = resultSet.getString(9);
-					
-					arrayList.add(new Book(bookId,bookName,bookPrice,bookAuthor,bookPublisher,bookPublishedDate,bookImage,bookQuantity,bookImageName));
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			finally
-			{
-				try
-				{
-					preparedStatement.close();
-					connection.close();
-				}
-				catch(SQLException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-		return arrayList;
-	}
-	
-	public static Book selectById(int id)
-	{
-		Connection connection = MyConnection.connect();
-		PreparedStatement preparedStatement = null;
-		Book book = null;
-		
-		if(connection != null)
-		{
-			String sql = "select bookId,bookName,bookPrice,bookAuthor,bookPublisher,bookPublishedDate,bookImageURL,bookQuantity,bookImageName from book where bookId=?";
-			try
-			{
-				preparedStatement = connection.prepareStatement(sql);
-				preparedStatement.setInt(1, id);
-				ResultSet resultSet = preparedStatement.executeQuery();
-				
-				while(resultSet.next())
-				{
-					int bookId = resultSet.getInt(1);
-					String bookName = resultSet.getString(2);
-					float bookPrice = Float.parseFloat(resultSet.getString(3));
-					String bookAuthor = resultSet.getString(4);
-					String bookPublisher = resultSet.getString(5);
-					String bookPublishedDate = resultSet.getString(6);
-					InputStream bookImage = resultSet.getBinaryStream(7);
-					int bookQuantity = resultSet.getInt(8);
-					String bookImageName = resultSet.getString(9);
-					System.out.println("Book Found : "+bookImageName);
-					
-					book = new Book(bookId,bookName,bookPrice,bookAuthor,bookPublisher,bookPublishedDate,bookImage,bookQuantity,bookImageName);
-				}
-			}
-			catch(SQLException e)
-			{
-				e.printStackTrace();
-			}
-			finally {
-				try
-				{
-					preparedStatement.close();
-					connection.close();
-				}
-				catch(SQLException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-		return book;
 	}
 }
